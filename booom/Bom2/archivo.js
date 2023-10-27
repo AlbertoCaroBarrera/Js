ef67d1f45be7fe2d6e6207d977c4ebc20a74b91e
Alberto Caro <albertocarobarrera@gmail.com>
2023-10-27 14:32:25 +0200

    octubre

---
+++ booom/Bom2/archivo.js
@@ -0,0 +1,11 @@
+
+// En un cuadro de diálogo de alerta, muestre el alto y ancho en píxeles y la profundidad de color de su pantalla. Cada valor será separado por un salto de línea.
+
+/*
+alert("La resolución de tu pantalla es: " + screen.width + " x " + screen.height + "y la profundidad de color es: " + screen.colorDepth) 
+*/
+
+// Crear un programa que va a crear un reloj digital en el título. Utilice el método setInterval() para actualizar el título de la página una vez cada minuto con la hora actual.
+
+
+
